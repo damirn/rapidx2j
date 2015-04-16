993f43f95bcda29ffd92293f6b0f4a6f6659f499
Damir Nedžibović <damirn@gmail.com>
2015-04-16 16:40:12 +0200

    Fix module name; looks like this prevented us from working on node 0.10.x

--- src/rapidx2j.cc
+++ src/rapidx2j.cc
@@ -5,4 +5,4 @@ void InitAll(v8::Handle<v8::Object> exports)
   exports->Set(NanNew<v8::String>("parse"), NanNew<v8::FunctionTemplate>(Parse)->GetFunction());
 }
 
-NODE_MODULE(addon, InitAll)
+NODE_MODULE(rapidx2j, InitAll)
