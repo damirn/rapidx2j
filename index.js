89ba0ac8ab80f1e76838f0569827c7ee884e51fe
Sander AKA Redsandro <redsandro@gmail.com>
2015-10-12 15:16:29 +0200

    Add callback. Fixes #3
    Added callback. Needs testing.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var native = require('./build/Release/rapidx2j');
 
-exports.parse = function(xml, options) {
+exports.parse = function(xml, options, cb) {
+  
+  // Parse options
   if (typeof options === 'undefined')
     options = { empty_tag_value: true, parse_int_numbers: true, parse_float_numbers: true, skip_parse_when_begins_with: '' };
   if (typeof options.empty_tag_value === 'undefined')
@@ -11,6 +13,22 @@ exports.parse = function(xml, options) {
     options.parse_float_numbers = true;
   if (typeof options.skip_parse_when_begins_with === 'undefined')
     options.skip_parse_when_begins_with = '';
-  var res = native.parse(xml, options);
-  return res;
+
+  // Async: Do we need to call back?    
+  if (typeof cb === 'function') {
+    process.nextTick(function() {
+      try {
+        result = native.parse(xml, options);
+        cb(null, result);
+      } catch (err) {
+        cb(err);
+      }
+    });
+  }
+  
+  // Sync response
+  else { 
+    var result = native.parse(xml, options);
+    return result;
+  }
 }
